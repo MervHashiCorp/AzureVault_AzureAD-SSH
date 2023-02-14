7d2ad073db77d909ea52190406c5e0f41ac0210c
MervHashiCorp <100760728+MervHashiCorp@users.noreply.github.com>
2023-02-14 09:15:06 +0200

    pip

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_security_group" "adssh" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "adssh-terraform_nic" {
+resource "azurerm_network_interface" "adssh" {
   name                = "adssh-nic"
   location            = azurerm_resource_group.adssh.location
   resource_group_name = azurerm_resource_group.adssh.name
