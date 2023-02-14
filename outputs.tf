1f5b3e93493ddb7db92610028a9234ad62c41d6a
MervHashiCorp <100760728+MervHashiCorp@users.noreply.github.com>
2023-02-14 12:39:21 +0200

    output resource local_file

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,9 @@ output "tls_private_key" {
   value     = tls_private_key.adssh.private_key_pem
   sensitive = true
 }
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.adssh.private_key_pem
+  filename        = "./id_rsa"
+  file_permission = "0600"
+}
