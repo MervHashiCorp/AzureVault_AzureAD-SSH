41a124b489c59fb358778f0cee51edcd2ef54e1d
MervHashiCorp <100760728+MervHashiCorp@users.noreply.github.com>
2023-02-16 15:01:13 +0200

    #94 removed local file resource

--- main.tf
+++ main.tf
@@ -92,11 +92,6 @@ resource "tls_private_key" "adssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-resource "local_file" "private_key" {
-  content         = tls_private_key.adssh.private_key_pem
-  filename        = ".ssh/id_rsa"
-  file_permission = "0600"
-}
 
 #Linux VM
 resource "azurerm_linux_virtual_machine" "adssh" {
