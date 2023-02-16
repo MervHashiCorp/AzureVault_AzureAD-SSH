a4715f70725d5c374f15c470402ce8fb78dde2ca
MervHashiCorp <100760728+MervHashiCorp@users.noreply.github.com>
2023-02-16 15:12:36 +0200

    #95 commented out local file

--- main.tf
+++ main.tf
@@ -92,11 +92,13 @@ resource "tls_private_key" "adssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+/*
 resource "local_file" "private_key" {
   content         = tls_private_key.adssh.private_key_pem
   filename        = "id_rsa"
   file_permission = "0600"
 }
+*/
 
 #Linux VM
 resource "azurerm_linux_virtual_machine" "adssh" {
